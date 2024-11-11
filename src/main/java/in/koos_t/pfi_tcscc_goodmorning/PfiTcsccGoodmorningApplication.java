51c26bd19eb502e6a30e08651473871e247aea28
Srinivas Yadav <kookatlasrinivasyadav@gmail.com>
2024-11-11 15:22:28 +0530

    updated src files

---
+++ src/main/java/in/koos_t/pfi_tcscc_goodmorning/PfiTcsccGoodmorningApplication.java
@@ -0,0 +1,13 @@
+package in.koos_t.pfi_tcscc_goodmorning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfiTcsccGoodmorningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfiTcsccGoodmorningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/koos_t/pfi_tcscc_goodmorning/PfiTcsccGoodmorningApplicationTests.java
@@ -0,0 +1,13 @@
+package in.koos_t.pfi_tcscc_goodmorning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfiTcsccGoodmorningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
